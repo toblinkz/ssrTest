8f9c5ef3a9b89a521e1f8f28fcb7324da61844c3
toblinkz <tobi.egbayelo@termii.com>
2022-04-22 13:48:50 +0100

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,9 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { 'http-equiv': 'Content-Security-Policy', content: "default-src 'self'; img-src * data:; object-src 'none'"},
+      { 'http-equiv': 'X-Frame-Options', content: "none"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
